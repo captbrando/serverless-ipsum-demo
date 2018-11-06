dab0fe7bb8d96a8b680637771d564e5db0361eab
Brando TEst <brwtest@debtest.brandoshouse.net>
2018-11-06 09:58:26 -0600

    First commit

---
+++ dictionary.js
@@ -0,0 +1,17 @@
+module.exports = [
+	'auto-scaling',
+	'zero-maintenance',
+	'pay-per-execution',
+	'serverdeath',
+	'function',
+	'event',
+	'handler',
+	'cloud',
+	'NoOps',
+	'Lambda',
+	'microservices',
+	'monitoring',
+	'synergy',
+	'machine-learning',
+	'artificial-intelligence',
+]
